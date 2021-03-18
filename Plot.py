ed7d7a14427c70c24bb974f6b4cad582b7c25b47
Guilhem Fouilhé <79322551+Guilhem-Fouilhe@users.noreply.github.com>
2021-03-18 19:40:05 +0100

    Changed dir

--- Plot.py
+++ Plot.py
@@ -2,19 +2,19 @@ import os, os.path
 import numpy as np
 import matplotlib.pyplot as plt
 
-os.chdir('C:\\V2\\train')
+os.chdir('C:\\Github\\plantnet_dataset\\plantnet_subset')
 x = np.zeros(len(os.listdir()))
 y = np.zeros(len(os.listdir()))
 
 i = 0
 for species in os.listdir():
-    DIR = 'C:\\V2\\train\\' + str(species)
+    DIR = 'C:\\Github\\plantnet_dataset\\plantnet_subset\\' + str(species)
     y[i] = len([name for name in os.listdir(DIR) if os.path.isfile(os.path.join(DIR, name))])
     x[i] = species
     i +=1
     # print(len([name for name in os.listdir(DIR) if os.path.isfile(os.path.join(DIR, name))]))
 
-#plt.plot(np.array(x),np.array(y))
+plt.plot(np.array(x),np.array(y))
 #print(x)
 #print(y)
 len(os.listdir())
