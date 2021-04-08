09891f495ef409afa608f1c595e3ad9aeb5034f1
Aurelien2021 <aurelien-mail@orange.fr>
2021-04-08 19:50:06 +0200

    Update of 08/04
    cf UpdatesReport

--- UpdatesReport.md
+++ UpdatesReport.md
@@ -5,6 +5,9 @@ https://docs.google.com/document/d/1SY5bn5L_R2vqLS5i3iNWATqsn_-3c4BE1hlDDt5Gbqk/
 Dataset (photos from Pl@ntnet) : https://gitlab.inria.fr/cgarcin/plantnet_dataset
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 Update 04/04/21 by Aurelien :
 We were told to write our scripts in English, so I don't know if we are also expected to write in english our reports. Finally, I got GithubDesktop, VisualStudioCode and Python to work. It was much harder than expected but it works now.
@@ -20,3 +23,21 @@ I have also tried to get a fitting curve of the exponential decay, by using the 
 That's basically what I did today.
 Aurelien
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+Update 08/04 by Aurelien :
+In order to complete the second step, I've learnt how to make subplots with matplotlib. The complexity of the display notably required to change the plt.plot for ax.plot and consequently to change all of the linked commands (for the graphs).
+
+Then, to get an interactive display, I looked into the Ipywidgets documentation, as we were invited to. It didn't seem to me extremely obvious that it could be the appropriate library in order to get the interactive display we wanted. It seemed to me that Ipywidget was great to modulate the scale, the axes, the printed function, but I didn't find how it could help us to modificate plants images (jpg) in real time. So I looked for something else.
+
+So I looked into Pygame documentation. I thought and I still think that it would be the best way to have an complicated interactive display in real time. Nevertheless, I still looked for something else since the Pygame code is a whole thing in itself, and I didn't want, if I had the possibility, to go directly into it. Also, I looked for something else because I thought that Pygame might not be a regular tool in order to complete a serious work.
+
+Then, I discovered FuncAnimation. It works well to get a 'simple' moving display. The only problem is that the documentation didn't seem great in general -in my opinion-, except for the Ken Hughes' article that I linked in the code.
+
+Eventually, I added a cursor that follows the mouse and I tried to obtain the coordinates of the mouse in real time with Button (both functions from matplotlib.widgets). Thus, mouse's coordinates are correctly printed for each click. But I can't get the mouse's coordinates into an array. I don't understand why it prints 'local variable 'MOUSE' referenced before assignment'.
+
+Current state : https://i.ibb.co/99rX13G/Capture-d-cran-2021-04-08-192809.png
+
+That's what I'll try to fix in the following days.
+
+If it's possible to save mouse's coordinates in an array. Then it should be quite easy to change the plant image in real time with FuncAnimation.
--- graph.py
+++ graph.py
@@ -1,10 +1,12 @@
-# %%
 ##Librairies
 
 import os
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+
+from matplotlib.widgets import Cursor, Button
+from matplotlib.animation import FuncAnimation
 
 ##Notes
 
@@ -14,11 +16,23 @@ import pandas as pd
 # for instance : C:\Users\lyz50\desktop should be :
 #  os.chdir('C:\\Users\\lyz50\\documents') ; then it works
 
+# Next step : get the coordinates of the mouse in real time
+# https://matplotlib.org/stable/users/event_handling.html maybe ?
+
+
 ##Documentation
 # https://www.youtube.com/watch?v=tJxcKyFMTGo for os
+# https://www.delftstack.com/fr/howto/matplotlib/add-subplot-to-a-figure-matplotlib/ for subplots
+# https://brushingupscience.com/2016/06/21/matplotlib-animations-the-easy-way/ for matplotlib.animation.FuncAnimation
+
+# https://realpython.com/python-matplotlib-guide/ for ax.plot instead of plt.plot
+# https://stackoverflow.com/questions/43482191/matplotlib-axes-plot-vs-pyplot-plot for ax.plot (cf 1st comment)
+# https://python4astronomers.github.io/plotting/advanced.html for ax.plot
+
+# https://www.youtube.com/watch?v=YobjoBrND4w for mouse's coordinates
 
 student1 = True #Aurélien 
-student2 = False  #Guilhem 
+student2 = False #Guilhem 
 if student1:
     path_to_train = os.path.join('C:','\\Users','lyz50','Documents','GitHub','plantnet_dataset','python','train')
 elif student2:
@@ -42,37 +56,98 @@ for i in range(len(x)):
         new_x += [x[i]]
         new_y += [y[i]]
 
-
 df = pd.DataFrame({"Species":new_x, "Images":new_y})
 df = df.sort_values("Images", ascending=False)
 df = df.reset_index(drop=True)
 #print(df)
 
-#Section (*) To get a standard graph
+####Plot the graph and the image
+DIR='1355868'
+path_to_DIR = os.path.join('C:\\','Users','lyz50','Documents','GitHub','plantnet_dataset','python','train',DIR)
+os.chdir(path_to_DIR)
+
+image=os.listdir()[5]
+read_image=plt.imread( os.path.join(path_to_DIR,image))
+
+fig=plt.figure()
+
+#Bar plot
+
+bar_width = 5
+
+first_sub_fig=fig.add_subplot(1, 1, 1)#position of the subplot
+subplot_title=("Number of images for each species")
+first_sub_fig.set_title(subplot_title)
+first_sub_fig.set_ylabel("Species")
+first_sub_fig.set_ylabel("Number of Images")
+first_sub_fig.bar(df.index, "Images", data=df, width=bar_width)
+
+cursor = Cursor(first_sub_fig, color='red', linewidth=2)#vertical and horizontal lines
+
+#Plant image
+second_sub_plot=fig.add_subplot(1, 2, 2)#position of the subplot
+subplot_title=("Subplot0")
+second_sub_plot.set_title(subplot_title)  
+plt.axis('off')
+plt.imshow(read_image)
+
+plt.show()
+
+####End of 'Plot the graph and the image'
+
+##Two Other Visualisations :
+
 array = df.to_numpy()  # array=([new_x_i,new_y_i]) and new_y is in sorted in descending order in df
 new_y_ordered = []
 for i in range(len(array)):
     new_y_ordered += [array[i][1]]
-#End of Section (*)
 
-bar_width = 0.8
+#To get the coordinates of the mouse in real time
+MOUSE=[[17,17]] #Global Variable
+print(MOUSE)
+print("Corretly printed")
+def onclick(event):
+    x,y=event.xdata, event.ydata
+    print(x,y)
+    #print(MOUSE)
+    #MOUSE+=[[x,y]]
+    #print("END OF ONLICK")
+#The First
 
-plt.clf()
-plt.bar(df.index, "Images", data=df, width=bar_width)
-plt.xlabel("Species")
-plt.ylabel("Number of Images")
-plt.title("Number of images for each species")
+fig=plt.figure()
+ax=fig.add_subplot(1, 1, 1)#position of the subplot
+ax.plot(df.index, new_y_ordered,"o", color="red")
+ax.set_xlabel("Species")
+ax.set_ylabel("Number of Images")
+ax.set_title("Number of images for each species")
+
+cursor = Cursor(ax,color='lightblue', linewidth=2)#vertical and horizontal lines
+
+second_sub_plot=fig.add_subplot(1, 2, 2)#position of the subplot
+subplot_title=("Subplot0")
+second_sub_plot.set_title(subplot_title)  
+plt.axis('off')
+plt.imshow(read_image)
+
+fig.canvas.mpl_connect('button_press_event', onclick)
+
+def animate(i):
+    subplot_title=('Species ' + str(i)+'(test)')
+    second_sub_plot.set_title(subplot_title)  
+anim = FuncAnimation(fig, animate, interval=100, frames=10)
+
+plt.draw()
 plt.show()
 
-plt.clf()
-plt.loglog(df.index, new_y_ordered / df["Images"].sum(),
-           "o", color="red", ms=3,
-           label="Plantnet distribution")
-plt.xlabel("Species")
-plt.ylabel("Number of Images")
-plt.axhline(y=1/len(df.index), linestyle="--", label="Uniform distribution")
-plt.title("Number of images for each species")
-plt.legend()
-plt.show()
+#The Second
 
-# %%
+# plt.clf()
+# plt.loglog(df.index, new_y_ordered / df["Images"].sum(),
+#            "o", color="red", ms=3,
+#            label="Plantnet distribution")
+# plt.xlabel("Species")
+# plt.ylabel("Number of Images")
+# plt.axhline(y=1/len(df.index), linestyle="--", label="Uniform distribution")
+# plt.title("Number of images for each species")
+# plt.legend()
+# plt.show()
