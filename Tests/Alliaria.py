5a1c779cfff99cda43fbb844b624b61826eda63b
Aurelien2021 <aurelien-mail@orange.fr>
2021-05-27 22:38:02 +0200

    Double
    The Alliaria_petiolata is in double in the dataset.

---
+++ Tests/Alliaria.py
@@ -0,0 +1,9 @@
+import os
+import pandas as pd
+
+url = 'https://raw.githubusercontent.com/GFouilhe-AThiriet/Projet-L3/main/Miscellaneous/groups.csv'
+data = pd.read_csv(url)
+
+for i in range (len(data)):
+    if data.species_group[i] == "Alliaria":
+        print(data.species_name[i],i)
