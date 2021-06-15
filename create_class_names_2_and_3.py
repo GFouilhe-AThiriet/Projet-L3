4452e08e1278f100e64d9bef8c418fed4b671b75
Aurelien2021 <aurelien-mail@orange.fr>
2021-06-15 11:23:06 +0200

    Merged the two files that create the new csv file

---
+++ create_class_names_2_and_3.py
@@ -0,0 +1,47 @@
+import os
+import csv
+import pandas as pd
+import numpy as np
+
+from functions import *
+
+User = "Aurélien"
+path_to_train , path_to_folder = user_paths(User)
+
+# # Transform class_names into class_names_2
+
+# Dataframe with id_species, species_name
+data = pd.read_csv(os.path.join('C:','\\Users','lyz50',"documents","Github","Projet-L3","class_names.csv"))
+
+species_name = data.species_name.to_numpy()
+
+L=[]
+for i in range (len(data)):
+    species_group=""
+    j=0
+    while species_name[i][j]!="_":
+        species_group=species_group+species_name[i][j]
+        j=j+1
+    L+=[species_group]
+
+group = pd.Series(L)
+
+data.insert(2, "genus", group)
+data.to_csv("class_names_2.csv",index=False)
+
+# Transform class_names_2 into class_names_3
+
+# Dataframe with id_species, species_name
+data = pd.read_csv(os.path.join(path_to_folder,"class_names_2.csv"))
+
+Images = []
+for i in range(len(data)):
+    id_species = data.id_species[i]
+    path_to_DIR = os.path.join(path_to_train,str(id_species))
+    os.chdir(path_to_DIR)
+    Images += [len(os.listdir())]
+
+os.chdir(path_to_folder)
+data.insert(3, "Images", Images)
+data.sort_values(by=['Images'], inplace=True, ascending=False)
+data.to_csv("class_names_3.csv",index=False)
